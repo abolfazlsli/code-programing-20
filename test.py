a93728f2d138c46961b52a36fa7d6d65d2610c33
Abolfazl Salehi <140882091+abolfazlsli@users.noreply.github.com>
2025-02-03 17:10:09 +0330

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,15 @@ def out():
 def enter():
     enterTime={"Enter":datetime.now()}
     return enterTime
+
+
+
+o=open("log.txt" , "a+")
+
+while True:
+    command = input("Enter the 1 to set Enter time and set 2 for exit time: ")
+    name = input ("Whats your name : ")
+    o.write("name: " + name)
+    if command == "1" or command == 1 : o.write(enter())
+    else : o.write(out())
+    o.close()
